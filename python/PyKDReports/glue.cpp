e55955589349f8ab5dd30115f229f1f952bfa72c
Allen Winter <allen.winter@kdab.com>
2022-09-01 10:26:25 -0400

    python/PyKDReports/glue.cpp - disable clang-format on snippet
    Don't let clang-format modify the snippet section else
    compile will fail.

--- python/PyKDReports/glue.cpp
+++ python/PyKDReports/glue.cpp
@@ -14,24 +14,29 @@
 **
 ****************************************************************************/
 
+// clang-format off
+// Don't reformat this snippet
+
 // @snippet abstract_table_element_default_font
 
 bool p;
-auto retval = % CPPSELF.% FUNCTION_NAME(&p);
-% PYARG_0 = PyTuple_New(2);
-PyTuple_SET_ITEM(% PYARG_0, 0, % CONVERTTOPYTHON[QFont](retval));
-PyTuple_SET_ITEM(% PYARG_0, 1, % CONVERTTOPYTHON[bool](p));
+auto retval = %CPPSELF.%FUNCTION_NAME(&p);
+%PYARG_0 = PyTuple_New(2);
+PyTuple_SET_ITEM(%PYARG_0, 0, %CONVERTTOPYTHON[QFont](retval));
+PyTuple_SET_ITEM(%PYARG_0, 1, %CONVERTTOPYTHON[bool](p));
 
 // @snippet abstract_table_element_default_font
 
 // @snippet report_get_margins
 qreal top, left, bottom, right;
 
-% CPPSELF.% FUNCTION_NAME(&top, &left, &bottom, &right);
-% PYARG_0 = PyTuple_New(4);
-PyTuple_SET_ITEM(% PYARG_0, 0, % CONVERTTOPYTHON[qreal](top));
-PyTuple_SET_ITEM(% PYARG_0, 1, % CONVERTTOPYTHON[qreal](left));
-PyTuple_SET_ITEM(% PYARG_0, 2, % CONVERTTOPYTHON[qreal](bottom));
-PyTuple_SET_ITEM(% PYARG_0, 3, % CONVERTTOPYTHON[qreal](right));
+%CPPSELF.%FUNCTION_NAME(&top, &left, &bottom, &right);
+%PYARG_0 = PyTuple_New(4);
+PyTuple_SET_ITEM(%PYARG_0, 0, %CONVERTTOPYTHON[qreal](top));
+PyTuple_SET_ITEM(%PYARG_0, 1, %CONVERTTOPYTHON[qreal](left));
+PyTuple_SET_ITEM(%PYARG_0, 2, %CONVERTTOPYTHON[qreal](bottom));
+PyTuple_SET_ITEM(%PYARG_0, 3, %CONVERTTOPYTHON[qreal](right));
 
 // @snippet report_get_margins
+
+// clang-format on
